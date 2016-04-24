8b7d1da1b274529f448418b927225eec30056031
zuoluo <zuoluo@aidaojia.com>
2016-04-24 21:40:27 +0800

    提交

--- PHPExport.php
+++ PHPExport.php
@@ -4,8 +4,31 @@
  * User: zuoluo
  * Date: 16/4/15
  * Time: 下午4:18
+ *
+ * 基于phpexcel导出
+ * 数据格式定义array:
+ * [ sheetTitle => [ columnIndex1 => [columnValue1, columnValue2]],
+ *                   columnIndex2 => [columnValue1, columnValue2]],
+ *                   columnIndex3 => [columnValue1, columnValue2]]
+ *                  ];
+ *
+ * [ 所有产品列表 => [  title => [产品Id, 产品编码],
+ *                    1252 =>  [1252, SG1251],
+ *                    1253 =>  [1253, SG1251]
+ *                  ];
+ *
+ * 表格标题定义:
+ * 可支持自定义传入,否则数据日期格式标题
+ * 构造方法中传入
+ * public function __construct($handleData, $excelName = '')
+ *
  */
 namespace Aidaojia\PHPExport;
+
+use PHPExcel;
+use PHPExcel_IOFactory;
+use PHPExcel_Style_Border;
+use PHPExcel_Style_Alignment;
 
 class PHPExport
 {
@@ -20,6 +43,12 @@ class PHPExport
      * @var array $handleDate
      */
     private $handleDate = [];
+
+    /**
+     *
+     * @var string $excelName
+     */
+    private $excelName = '';
 
     /**
      *
@@ -38,11 +67,18 @@ class PHPExport
      *
      * @param $handleData
      */
-    public function __construct($handleData)
+    public function __construct($handleData, $excelName = '')
     {
-        if ( ! $handleData ) {
+        if ( $handleData ) {
             $this->handleDate = $handleData;
         }
+
+        if ( $excelName ) {
+            $this->excelName = $excelName;
+        } else {
+            $this->excelName = date('Y-m-d', time()) . '表格导出';
+        }
+
         $this->excel = new PHPExcel();
     }
 
@@ -52,22 +88,33 @@ class PHPExport
     public function getExcelByData()
     {
         if ( ! $this->handleDate ) {
-            $this->excel;
+            return $this->excel;
         }
 
         foreach ($this->handleDate as $key => $val) {
             // active sheet index
-            $this->excel->setActiveSheetIndex($this->sheetIndex);
+            $this->excel->setActiveSheetIndex($this->sheetIndex ++);
 
             // get sheet by given data
             $this->_getSheetByData($this->excel, $key, $val);
+
+            $this->excel->createSheet();
+        }
+
+        if (!headers_sent()) {
+            header('Content-Type: application/vnd.ms-excel');
+            header('Content-Disposition: attachment;filename=' . $this->excelName . '.xlsx');
+            header('Cache-Control: max-age=0');
+            $output = PHPExcel_IOFactory::createWriter($this->excel, 'Excel2007');
+            $output->save('php://output');
+            exit;
         }
     }
 
     /**
      *  PHPExport _getSheetByData
      */
-    private function _getSheetByData($excel, $sheetTitle, $sheetValue)
+    private function _getSheetByData(& $excel, $sheetTitle, $sheetValue)
     {
         // get common style
         $commonStyle = $this->_getCommonStyle();
@@ -80,7 +127,7 @@ class PHPExport
         // set A1 cell param
         $sheet->getStyle('A1')->getFont()->setBold(true);
         $length = count($sheetValue['title']);
-        $endColumn = getMergeColumn($length-1);
+        $endColumn = $this->_getMergeColumn($length-1);
         $sheet->mergeCells("A1:{$endColumn}1");
         $sheet->getStyle("A1:{$endColumn}1")->applyFromArray($commonStyle);
         $sheet->getRowDimension('1')->setRowHeight(20);
@@ -90,7 +137,6 @@ class PHPExport
         // generate sheet content not include A1 cell
         $this->_getSheetContent($sheet, $sheetValue);
 
-        return $excel;
     }
 
     private function _getSheetContent(& $sheet, $sheetValue)
@@ -102,7 +148,7 @@ class PHPExport
         $this->columnIndex = 1;
         foreach ($sheetValue as $key => $val) {
 
-            ++ $this->columnIndex;
+            $this->columnIndex ++;
             $columnLength = count($val);
 
             for($i = 0; $i < $columnLength; $i++) {
@@ -172,3 +218,4 @@ class PHPExport
 
 
 }
+
